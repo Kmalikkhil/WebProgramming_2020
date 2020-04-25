c08c42cc2c05a2a2f8b087efdc981c3acb4c202c
Kanishka Malikkhil <103220762@student.swin.edu.au>
2020-04-25 16:10:31 +1000

    added animation to images

--- JavaScript/CookieClicker/cookie.js
+++ JavaScript/CookieClicker/cookie.js
@@ -3,47 +3,62 @@ let cookieimg = document.getElementById("cookieImg");
 let pointerImg = document.getElementById("pointerImg");
 let numberOfCookiesPerSecond = document.getElementById("numberOfCookiesPerSecond");
 let pointerValue = document.getElementById("pointerValue");
-let upgradePurchased = document.getElementById("upgradePurchased");
+let upgradesPurchased = document.getElementById("upgradesPurchased");
 let cookieCounter = 0;
 let cookieCounterPerSecond = 0;
 let buyingAbility = 10;
 
+
+// Onclick event for upgrade pointer
 pointerImg.onclick = upgradepointer;
 
 function upgradepointer(){
 
     
-    
+// Condition to meet the purchase of an upgrade    
     if(cookieCounter >= buyingAbility)
     {
         cookieCounterPerSecond += 1
         numberOfCookiesPerSecond.innerHTML = "Per Second " + cookieCounterPerSecond;
         cookieCounter -= buyingAbility;
         numberOfCookies.innerHTML = cookieCounter + " Cookies";
-        //let newdiv = document.createElement("div");
-        //upgradePurchased.appendChild(newdiv);
         
+        // setting the timer to add a cookie
 
         setInterval(function() {
             cookieCounter += cookieCounterPerSecond;
             numberOfCookiesPerSecond.innerHTML = "Per Second " + cookieCounterPerSecond;
             numberOfCookies.innerHTML = cookieCounter + " Cookies";
 
-
+        // 1000 means 1 second in milliseconds
         },1000);
+
+        // increases the price to purchase an upgrade
         buyingAbility += 10;
+
+        // caption: current price of the upgrade
         pointerValue.innerHTML = "Worth: " + buyingAbility + " Cookies";
-       
-        //upgradePurchased.innerHTML = pointerImg;
+        
+        //showing all the upgrades purchased so far
+        let newFig = document.createElement("figure");
+        let newImg = document.createElement("img")
+        
+        let newcaption = document.createElement("figcaption");
+        newcaption.innerHTML = cookieCounterPerSecond + " Cookie/Sec";
+        newImg.src = "pointer.png";
+
+        upgradesPurchased.appendChild(newFig);
+        newFig.appendChild(newImg);
+        newFig.appendChild(newcaption);
+        
+        
     }
     
     
 }
 
 
-
-
-
+//Function for clicking on Cookie image
 cookieimg.onclick = cookieClick;
 
 function cookieClick(){
